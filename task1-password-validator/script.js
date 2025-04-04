708490b5b18079a9b343843d7de5f7f9dc7176f3
d-manucharyan <davit.manucharian@gmail.com>
2025-04-04 11:37:16 +0400

    Adding task 1 solution

--- task1-password-validator/script.js
+++ task1-password-validator/script.js
@@ -1,1 +1,51 @@
-alert("Hello");
+
+function validatePassword(password){
+let minLength = password.length >= 8;
+let hasUpperCase = false;
+let hasLowerCase = false;
+let hasNumber = false;
+let hasSpecials = false;
+
+let specials = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+for(let i=0; i < password.length; i++) {
+    let char = password[i];
+
+if(char >= "A" && char <= "Z") {
+  hasUpperCase=true;
+ }
+else if(char >= "a" && char <= "z") {
+  hasLowerCase=true;
+ }
+else if(char >= "0" && char <= "9") {
+  hasNumber = true;  
+ }
+else if(specials.includes(char)) {
+  hasSpecials = true
+ }
+ }
+
+ let isValid = minLength && hasUpperCase && hasLowerCase && hasNumber && hasSpecials;
+
+ let errors = []
+ if(!minLength) errors.push("Password must include at least 8 characters");
+ if(!hasUpperCase) errors.push("Password must include at least one uppercase letter");
+ if(!hasLowerCase) errors.push("Password must include at least one lowercase letter");
+ if(!hasNumber) errors.push("Password must include at least one lowercase letter");
+ if(!hasSpecials) errors.push("Password must include at least one lowercase letter");
+
+ return {
+    isValid , 
+    errors 
+  };
+
+}
+
+let password = prompt("Enter your password");
+let result = validatePassword(password);
+if (result.isValid) {
+    alert("Password is Valid");
+} else {
+    alert(result.errors
+    )
+}
